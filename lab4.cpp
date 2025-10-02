1f2e11ac9b992646bae24a34446adb28b1f050f3
jacobrak <155147415+jacobrak@users.noreply.github.com>
2025-10-02 23:00:17 +0200

    index_count

--- lab4.cpp
+++ lab4.cpp
@@ -2,10 +2,13 @@
 
 using namespace std;
 
-int main(int argc, char* argv[]){
-    if (argc != 3){
-        cerr << "wrong output";
-    }
+int index_count(int length){
+    int index = (length * 0.1) - 1;
+    return index;
+}
 
+int main(int argc, char* argv[]){
+
+    cout << index_count(20) << endl;
     return 0;
 }
