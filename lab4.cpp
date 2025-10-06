da22b221ad9fc3de7c982eb726df5752dbc1cc2d
jacobrak <155147415+jacobrak@users.noreply.github.com>
2025-10-06 14:13:41 +0200

    read

--- lab4.cpp
+++ lab4.cpp
@@ -4,20 +4,28 @@
 #include <string>
 using namespace std;
 
+
 int read_file(string& file_path){
     ifstream file(file_path);
+    
+    string line;
+    while (getline(file, line)){
+        cout << line << endl;
+    }
+    file.close();
+    return 0;
 }
 
 int vector(){
-
+    return 0;
 }
 
 int array(){
-
+    return 0;
 }
 
 int linked_list(){
-
+    return 0;
 }
 
 int index_count(int length){
@@ -26,7 +34,12 @@ int index_count(int length){
 }
 
 int main(int argc, char* argv[]){
-
+    if (argc < 2){
+        cerr << "wrong output fromat should be: " << "Usage, " << "filename" << endl;
+        return 1;
+    }
+    string path = argv[2];
+    read_file(path);
     cout << index_count(20) << endl;
     return 0;
 }
