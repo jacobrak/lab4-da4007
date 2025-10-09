2a4fd3b2963cd18d1e04014e1050821f7d30e0d3
jacobrak <155147415+jacobrak@users.noreply.github.com>
2025-10-10 00:52:01 +0200

    done for today ...

--- lab4.cpp
+++ lab4.cpp
@@ -10,21 +10,31 @@ int read_file(string& file_path){
     
     string line;
     while (getline(file, line)){
-        cout << line << endl;
+        return stoi(line);
     }
     file.close();
     return 0;
 }
-
-int vector(){
+vector<float> read_vector(string& file_path){
+    ifstream file(file_path);
+    vector<float> data;
+    string line;
+    while (getline(file, line)){
+        data.push_back(stof(line));
+    }
+    
+    file.close();
+    return data;
+}
+int use_vector(int data){
     return 0;
 }
 
-int array(){
+int use_array(int data){
     return 0;
 }
 
-int linked_list(){
+int use_linked_list(int data){
     return 0;
 }
 
@@ -34,12 +44,30 @@ int index_count(int length){
 }
 
 int main(int argc, char* argv[]){
-    if (argc < 2){
+    if (argc < 3){
         cerr << "wrong output fromat should be: " << "Usage, " << "filename" << endl;
         return 1;
     }
     string path = argv[2];
-    read_file(path);
+    int data = read_file(path);
+    
+    string method = argv[1];
+    cout << method << endl;
+    if (method == "vector"){
+        auto data = read_vector(path);
+            for (auto value : data) {
+                cout << value << " ";
+            }
+        return 0;
+    }
+    if (method == "array"){
+        use_array(data);
+        return 0;
+    }
+    if (method == "list"){
+        use_linked_list(data);
+        return 0;
+    }
     cout << index_count(20) << endl;
     return 0;
 }
