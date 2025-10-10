b9e674cc641a6bc10c3edb71a0891d17aefd74d2
jacobrak <155147415+jacobrak@users.noreply.github.com>
2025-10-10 15:21:58 +0200

    fix

--- lab4.cpp
+++ lab4.cpp
@@ -4,10 +4,13 @@
 #include <string>
 #include <algorithm>
 #include <cmath>
+#include <array>
 using namespace std;
+// Hyperparameter
+constexpr int MAX_SIZE_10M = 10000000;
 
 bool compare_func(const float& a, const float& b){
-    return a > b;
+    return a < b;
 }
 
 int index_count(int length){
@@ -18,16 +21,6 @@ int index_count(int length){
     return index;
 }
 
-int read_file(string& file_path){
-    ifstream file(file_path);
-    
-    string line;
-    while (getline(file, line)){
-        return stoi(line);
-    }
-    file.close();
-    return 0;
-}
 vector<float> read_vector(string& file_path){
     ifstream file(file_path);
     vector<float> data;
@@ -35,23 +28,38 @@ vector<float> read_vector(string& file_path){
     while (getline(file, line)){
         data.push_back(stof(line));
     }
-    
+
     file.close();
     return data;
 }
 
-int use_vector(vector<float>& data){
+float use_vector(vector<float>& data){
     sort(data.begin(), data.end(), compare_func);
     int array_size = data.size();
     int valid_index = index_count(array_size);
 
-
-
     return data[valid_index];
 }
+// allocate 10m for largest dataset
+int read_array(string& file_path, array<float, MAX_SIZE_10M>& data){
+    ifstream file(file_path);
+    
+    int count = 0;
+    string line;
+    while (getline(file, line)){
+        data[count] = stof(line);
+        count += 1;
+    }
 
-int use_array(int data){
-    return 0;
+    file.close();
+    return count;
+}
+
+float use_array(array<float, MAX_SIZE_10M>& data, int count){
+    sort(data.begin(), data.begin() + count, compare_func);
+    int valid_index = index_count(count);
+
+    return data[valid_index];
 }
 
 int use_linked_list(int data){
@@ -66,23 +74,21 @@ int main(int argc, char* argv[]){
         return 1;
     }
     string path = argv[2];
-    int data = read_file(path);
+   
     
     string method = argv[1];
-    cout << method << endl;
-    cout << index_count(9) << endl;
     if (method == "vector"){
         auto data = read_vector(path);
-        use_vector(data);
-
+        cout << use_vector(data) << endl;
         return 0;
     }
     if (method == "array"){
-        use_array(data);
+        static array<float, 10000000> data;
+        int count = read_array(path, data);
+        cout << use_array(data, count) << endl;
         return 0;
     }
     if (method == "list"){
-        use_linked_list(data);
         return 0;
     }
     
